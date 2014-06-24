1b9bbdb9dddc04e3ab5ac3d2e556bf542fb89c8d
Kazuya Yoshioka <fritillaria.imperialis1023@gmail.com>
2014-06-24 17:53:08 +0900

    Updated: Node.java  -KazuyaYoshioka at[2014年 6月24日 火曜日 17時53分08秒 JST]

--- Program/Coding_Files/forest/Node.java
+++ Program/Coding_Files/forest/Node.java
@@ -3,33 +3,39 @@ package forest;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ *樹状グラフにおける1つのノードを表すクラス。
+ *@author KazuyaYoshioka
+ */
 public class Node extends Object
 {
     /**
-       このノードを一意に決定する番号。
+     *このノードを一意に決定する番号。
     */
     private int nodeId;
     /**
-       このノードの親ノード。
+     *このノードの親ノード。
     */
     private Node parent;
     /**
-       このノードの子ノードのリスト。
+     *このノードの子ノードのリスト。
     */
     private List<Node> children;
     /**
-      このノードが属する木。
+     *このノードが属する木。
     */
     //private Tree tree;
     /**
-       このノードの名前。
+     *このノードの名前。
     */ 
     private String label;
 
     /**
-       インスタンスを生成して応答する。
-       このノードのノード番号に指定されあ整数を、ラベルに指定された文字列を設定し、 
-       親ノード、子ノードリストをそれぞれ初期化しておく。
+     *インスタンスを生成して応答する。
+     *このノードのノード番号に指定されあ整数を、ラベルに指定された文字列を設定し、 
+     *親ノード、子ノードリストをそれぞれ初期化しておく。
+     *@param anId ノード番号
+     *@param aLabel ラベル内容
     */
     public Node(int anId, String aLabel)
     {
@@ -41,11 +47,14 @@ public class Node extends Object
     }
     
     /**
-       インスタンスを生成して応答する。
-       このノードのノード番号に指定された整数を、ラベルに指定された文字列を、 
-       親ノードに指定されたノードを設定し、
-       子ノードリストを初期化しておく。
-    */ 
+     *インスタンスを生成して応答する。
+     *このノードのノード番号に指定された整数を、ラベルに指定された文字列を、 
+     *親ノードに指定されたノードを設定し、
+     *子ノードリストを初期化しておく。
+     *@param anId ノード番号
+     *@param aLabel ラベル内容
+     *@param aParentNode 親ノード
+     */
     public Node(int anId, String aLabel, Node aParentNode)
     {
 	super();
@@ -55,10 +64,14 @@ public class Node extends Object
 	this.children = new ArrayList<Node>();
     }
     /**
-       インスタンスを生成して応答する。
-       このノードのノード番号に指定された整数を、ラベルに指定された文字列を、 
-       親ノードと子ノードリストに指定されたノード及びリストを設定する。
-    */
+     *インスタンスを生成して応答する。
+     *このノードのノード番号に指定された整数を、ラベルに指定された文字列を、 
+     *親ノードと子ノードリストに指定されたノード及びリストを設定する。
+     *@param anId ノード番号
+     *@param aLabel ラベル内容
+     *@param aParentNode 親ノード
+     *@param aChildrenList 子ノードリスト
+     */
     public Node(int anId, String aLabel, Node aParentNode, ArrayList<Node> aChildrenList)
     {
 	super();
@@ -69,7 +82,8 @@ public class Node extends Object
     }
 
     /**
-       子ノードリストに子供を追加する。
+     *子ノードリストに子供を追加する。
+     *@param aChildNode 追加する子ノード 
     */
     public void addChildNode(Node aChildNode)
     {
@@ -78,7 +92,8 @@ public class Node extends Object
 	return;
     }
     /**
-       このノードの子ノードの数を応答する。
+     *このノードの子ノードの数を応答する。
+     *@return 子ノードの数
      */
     public int getNumberOfChildren()
     {
@@ -86,21 +101,24 @@ public class Node extends Object
     }
 
     /**
-       このノードのノード番号を応答する
-    */
+     *このノードのノード番号を応答する。
+     *@return ノード番号
+     */
     public int getNodeId()
     {
 	return this.nodeId;
     }
     /**
-       このノードの親ノードを応答する。
+     *このノードの親ノードを応答する。
+     *@return 親ノード 
     */
     public Node getParent()
     {
 	return this.parent;
     }
     /**
-       このノードの子ノードのリストを応答する。
+     *このノードの子ノードのリストを応答する。
+     *@return 子ノードのリスト 
     */
     public List<Node> getChildren()
     {
@@ -108,7 +126,9 @@ public class Node extends Object
     }
 
     /**
-       引数のノードとこのノードの一致性を応答する。
+     *引数のノードとこのノードの一致性を応答する。
+     *@param aNode 比較対象となるノード
+     *@return 一致した場合はtrue、それ以外はfalse
     */
     public boolean equals(Node aNode)
     {
@@ -117,7 +137,9 @@ public class Node extends Object
 	else{ return false; }
     }
     /**
-       引数のノード番号とこのノードのノード番号の一致性を応答する。
+     *引数のノード番号とこのノードのノード番号の一致性を応答する。
+     *@param aNodeId 比較対象となるノード番号
+     *@return 一致した場合はtrue、それ以外はfalse 
     */
     public boolean equals(int aNodeId)
     {
