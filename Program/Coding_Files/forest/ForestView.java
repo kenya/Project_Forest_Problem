34cf0b1a6bc14009db535758dac96f0deeb2001f
snk kny <shinkikenya@kny-no-MacBook-Air.local>
2014-06-30 15:58:10 +0900

    未完成

---
+++ Program/Coding_Files/forest/ForestView.java
@@ -0,0 +1,60 @@
+package forest;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+//import java.awt.Point;
+
+/**
+ * BranchとNodeを描くためのView
+ * @author kenya shinki
+ */
+public class ForestView extends mvc.View
+{
+	/**
+	 * 指定されたモデルでMVCを構築するコンストラクタ。
+     * @param aModel モデル
+	 */
+	public ForestView(ForestModel aModel)
+	{
+		super(aModel, new ForestController());
+	}
+	
+	/**
+	 * 指定されたモデルとコントローラでMVCを構築するコンストラクタ。
+     * @param aModel モデル
+     * @param aController コントローラー
+	 */
+	public ForestView(ForestModel aModel, ForestController aController)
+	{
+		super(aModel, aController);
+	}
+	
+	/**
+	 * 描画を行う。
+     * @param aGraphics グラフィックス
+	 */
+	public void paintComponent(Graphics aGraphics)
+	{
+        super.paintComponent(aGraphics);
+			// フォントの処理かな。画像に波の説明を書くこと
+        FourierPaneModel aModel = (FourierPaneModel) this.getModel();
+        Font aFont = new Font(Font.Serif, Font.PLAIN, 12);
+        aGraphics.setColor(Color.white);
+        aGraphics.setFont(aFont);
+        int fontHeight = aFont.getSize();
+        /*aGraphics.drawString(aModel.label(), 1, fontHeight + 1);
+        aGraphics.drawString(aModel.label(), 1, fontHeight + 2);
+        aGraphics.drawString(aModel.label(), 1, fontHeight + 3);
+        aGraphics.drawString(aModel.label(), 2, fontHeight + 1);
+        aGraphics.drawString(aModel.label(), 2, fontHeight + 2);
+        aGraphics.drawString(aModel.label(), 2, fontHeight + 3);
+        aGraphics.drawString(aModel.label(), 3, fontHeight + 1);
+        aGraphics.drawString(aModel.label(), 3, fontHeight + 2);
+        aGraphics.drawString(aModel.label(), 3, fontHeight + 3);*/
+		
+        aGraphics.setColor(Color.black);
+        aGraphics.drawString(aModel.label(), 2, fontHeight + 2);
+		return;
+	}
+}
